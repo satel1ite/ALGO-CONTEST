af19328bc4a756f0a78177f08ed15efc956bdaa2
satel1ite <chernyavskiy.as@gmail.com>
2026-03-09 20:55:19 +0300

    Create task_E.cpp

---
+++ contest6/task_E.cpp
@@ -0,0 +1,31 @@
+import sys
+
+
+n, m = map(int, sys.stdin.readline().split())
+
+weights = list(map(int, sys.stdin.readline().split()))
+costs = list(map(int, sys.stdin.readline().split()))
+
+dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    cur_w = weights[i - 1]
+    cur_c = costs[i - 1]
+    for j in range(m + 1):
+        if j >= cur_w:
+            if dp[i - 1][j - cur_w] + cur_c > dp[i - 1][j]:
+                dp[i][j] = dp[i - 1][j - cur_w] + cur_c
+            else:
+                dp[i][j] = dp[i - 1][j]
+        else:
+            dp[i][j] = dp[i - 1][j]
+
+res_indices = []
+curr_w = m
+for i in range(n, 0, -1):
+    if dp[i][curr_w] != dp[i - 1][curr_w]:
+        res_indices.append(i)
+        curr_w -= weights[i - 1]
+
+for idx in sorted(res_indices):
+    print(idx)
