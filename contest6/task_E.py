908deeeb61387d3b0367de28d3318eeb7ae59a88
satel1ite <chernyavskiy.as@gmail.com>
2026-04-15 13:09:37 +0300

    Update task_E.py

--- contest6/task_E.py
+++ contest6/task_E.py
@@ -13,10 +13,7 @@ for i in range(1, n + 1):
     cur_c = costs[i - 1]
     for j in range(m + 1):
         if j >= cur_w:
-            if dp[i - 1][j - cur_w] + cur_c > dp[i - 1][j]:
-                dp[i][j] = dp[i - 1][j - cur_w] + cur_c
-            else:
-                dp[i][j] = dp[i - 1][j]
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - cur_w] + cur_c)
         else:
             dp[i][j] = dp[i - 1][j]
 
@@ -27,5 +24,5 @@ for i in range(n, 0, -1):
         res_indices.append(i)
         curr_w -= weights[i - 1]
 
-for idx in sorted(res_indices):
+for idx in reversed(res_indices):
     print(idx)
